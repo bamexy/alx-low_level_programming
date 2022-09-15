b7a374178daa0f24c6ece99a0cc015d732278388
bamexy <ajibadematthew@gmail.com>
2022-09-15 16:03:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	_putchar(72);
-	_putchar(111);
-	_putchar(108);
-	_putchar(98);
-	_putchar(101);
-	_putchar(114);
-	_putchar(116);
-	_putchar(111);
-	_putchar(110);
-	_putchar ('\n');
 	return (0);
+
 }
